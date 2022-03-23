8fc6e419e59cda512f147279e471732a6d072f59
Igor Popov <igor@igorpopov.io>
2022-03-23 19:52:30 +0200

    Fix tests
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -13,7 +13,6 @@ namespace Tests
         {
             var parser = new QueryParameterEmbedder();
             var sb = new StringBuilder();
-            sb.Append("2022-03-12 12:34:56,795;");
             sb.Append("SELECT this_.Id as y0_ FROM Person this_ WHERE Id = @p1;");
             sb.Append("SELECT last_insert_rowid();");
             sb.Append("@p1 = 1 [Type: Int32 (0:0:0)]");
@@ -29,7 +28,6 @@ namespace Tests
         {
             var parser = new QueryParameterEmbedder();
             var sb = new StringBuilder();
-            sb.Append("2022-03-12 12:34:56,795;");
             sb.Append("SELECT this_.Id as y0_ FROM Person this_ WHERE Id IN (@p1, @p1234);");
             sb.Append("@p1 = 1 [Type: Int32 (0:0:0)], @p1234 = 2 [Type: Int32 (0:0:0)]");
 
